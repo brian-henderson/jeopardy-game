c6abda20deb3fc14b2096b1392402b18864287bc
Brian Henderson <brian.henderson1@marist.edu>
2018-04-16 11:00:57 -0400

    removed node modules and code components

--- index.js
+++ index.js
@@ -21,36 +21,8 @@ $(document).ready(function() {
     $("#categories-header").append("<div class='col s12 l3' id='category-header'><p>"+cat+"</p></div>");
   });
 
-  csv
-   .fromPath("data.csv", {trim: true})
-   .on("data", function(row){
-       //console.log(row);
-       var temp = new Question(index, row[0], row[1], row[2]);
-       cells[index] = temp;
-       index++;
-   })
-   .on("end", function(){
-       console.log("done");
-       cells.forEach(function(p) {
-         console.log(p);
-       });
-   });
-/*
-  $("#question-grid").append("<div class='col s12 l3' id="+'loc'+10+"> <a class='btn-flat btn-large' id="+10+">"+cell10.points+"</a> </div>");
-  $("#question-grid").append("<div class='col s12 l3' id="+'loc'+20+"> <a class='btn-flat btn-large' id="+20+">"+cell20.points+"</a> </div>");
 
-  $(".btn-flat").click(function(){
-    var id = $(this).attr('id');
-    var cell = eval("cell" + id);
 
-    if (!cell.opened) {
-      $(this).replaceWith("<a class='btn-flat btn-large' onclick='selected("+id+")' id="+id+">"+cell.question+"</a>");
-      cell.opened = true;
-    }
-    console.log(cell);
-
-  });
-*/
 });
 
 
