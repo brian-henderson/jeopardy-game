4c946f401e4e17d31742852bd7db8dabaa608f7e
Brian Henderson <brian.henderson1@marist.edu>
2018-04-16 10:34:20 -0400

    Scratchting node, attempting to use papa parse for in browser csv parsing

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var $ = require('jquery');
+
 var categories = ["Category-1", "Category-2", "Category-3", "Category-4"];
+var cells = [];
+var index = 0;
+var csv = require("fast-csv");
+var fs = require("fs");
+
 
 function Question (p, q, a) {
   this.points = p;
@@ -8,19 +15,30 @@ function Question (p, q, a) {
   this.answered = false;
 }
 
-var cell10 = new Question(200, "question 1", "answer 1");
-var cell20 = new Question(200, "question 2", "answer 2");
-
 $(document).ready(function() {
 
   categories.forEach(function(cat){
     $("#categories-header").append("<div class='col s12 l3' id='category-header'><p>"+cat+"</p></div>");
   });
 
+  csv
+   .fromPath("data.csv", {trim: true})
+   .on("data", function(row){
+       //console.log(row);
+       var temp = new Question(index, row[0], row[1], row[2]);
+       cells[index] = temp;
+       index++;
+   })
+   .on("end", function(){
+       console.log("done");
+       cells.forEach(function(p) {
+         console.log(p);
+       });
+   });
+/*
   $("#question-grid").append("<div class='col s12 l3' id="+'loc'+10+"> <a class='btn-flat btn-large' id="+10+">"+cell10.points+"</a> </div>");
   $("#question-grid").append("<div class='col s12 l3' id="+'loc'+20+"> <a class='btn-flat btn-large' id="+20+">"+cell20.points+"</a> </div>");
 
-/*
   $(".btn-flat").click(function(){
     var id = $(this).attr('id');
     var cell = eval("cell" + id);
@@ -35,9 +53,8 @@ $(document).ready(function() {
 */
 });
 
-function selected(id) {
-  alert($(id).attr('id'));
-};
+
+
 
 
 
