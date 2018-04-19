b395eafcddf5586315ffaed4adc48e3265a366d9
Brian Henderson <brian.henderson1@marist.edu>
2018-04-19 00:18:52 -0400

    Style changes and a few tag modifications

--- index.js
+++ index.js
@@ -16,16 +16,17 @@ $(document).ready(function() {
   for (var i=0; i<data.length; i++) {
     if (i == 0) {
       for (var j=0; j<data[i].length;j++) {
-        $("#categories-header").append("<div class='col s12 l3' id='category-header'><p>"+data[i][j]+"</p></div>");
+        $("#categories-header").append("<div class='col s12 l3' id='category-header'><p>"+data[i][j].toUpperCase()+"</p></div>");
       }
     }
     else {
       cells[i] = new Question (i, data[i][0], data[i][1], data[i][2]);
-      $("#question-grid").append("<div class='col s12 l3' id="+'loc'+i+"> <a class='btn-flat btn-large cell' id="+i+">"+cells[i].points+"</a> </div>");
+    //  $("#question-grid").append("<div class='col s12 l3 cell-div' id='cell-div'> <a class='btn-flat btn-large cell' id="+i+">"+cells[i].points+"</a> </div>");
+    $("#question-grid").append("<div class='col s12 l3 cell-div' id="+i+">$"+cells[i].points+"</div>");
     }
   };
 
-  $(".cell").click(function(){
+  $(".cell-div").click(function(){
     var id = $(this).attr('id');
     $('#modal-header').empty();
     $('#modal-body').empty();
@@ -34,7 +35,7 @@ $(document).ready(function() {
     $('#modal-body').append("<h4 id='a'>"+cells[id].answer+"</h4>")
     $('#a').hide();
     $('#modal-window').modal("open");
-    $( "#"+id ).last().addClass( "disabled" );
+    //$( "#"+id ).last().addClass( "disabled" );
     $( "#"+id ).empty();
   });
 
