d407f5e6dcbf79a59b88d51cae47ae2b0ff22a10
Brian Henderson <brian.henderson1@marist.edu>
2018-04-24 10:27:57 -0400

    Removed dead code

--- js/index.js
+++ js/index.js
@@ -12,41 +12,9 @@ $(document).ready(function() {
   $("#modal-initial").modal("open");
   $('.chips').chips();
 
-
-  /*
-    function createTable() {
-      for (var i = 0; i < data.length; i++) {
-        if (i == 0) {
-          for (var j = 0; j < data[i].length; j++) {
-            $("#categories-header").append("<div class='col s12 l3' id='category-header'><p>" + data[i][j].toUpperCase() + "</p></div>");
-          }
-        } else {
-          cells[i] = new Question(i, data[i][0], data[i][1], data[i][2]);
-          $("#question-grid").append("<div class='col s12 l3 cell-div' id=" + i + ">$" + cells[i].points + "</div>");
-        }
-      };
-    }
-
-
-    $(".cell-div").click(function() {
-      var id = $(this).attr('id');
-      $('#modal-header').empty();
-      $('#modal-body').empty();
-      $('#modal-header').append("<h3>$" + cells[id].points + "</h3>")
-      $('#modal-body').append("<h4 id='q'>" + cells[id].question + "</h4>")
-      $('#modal-body').append("<h4 id='a'>" + cells[id].answer + "</h4>")
-      $('#a').hide();
-      $('#modal-window').modal("open");
-      $("#" + id).empty();
-    });
-
-    */
-
   $("#populate").click(function() {
-    //    var csvFile = $("#csv-input").prop("files");
-    //    handleFile(csvFile);
-    //console.log("*************************");
-    //console.log(data);
+    console.log("pop")
+    $('.modal').modal("close");
   });
 
   $("#modal-body").click(function() {
@@ -64,7 +32,6 @@ $(document).ready(function() {
   $("#csv-input").change(function() {
     var csvFile = $("#csv-input").prop("files");
     handleFile(csvFile);
-    //console.log("*************************");
     console.log(data);
   });
 
